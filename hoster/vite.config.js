a12bf44f16f4ed4d2ffb32acbddbf95666e06349
Ayush Patidar <ayushpatidar814@gmail.com>
2025-11-23 22:44:08 +0530

    b

--- hoster/vite.config.js
+++ hoster/vite.config.js
@@ -4,12 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(
-    {
-      jsxRuntime: 'automatic',
-      babel: {
-        plugins: ["@babel/plugin-transform-react-jsx"],
-      },
-    }
-  )],
+  plugins: [tailwindcss(), react()],
 })
